941ca0c3c9a1a6d1cc7bd5d1f6d77d7fc70f2647
Asif Mansoori <asif.mansoori@mail.valuecoders.com>
2022-09-17 14:13:38 +0530

    fixing validation issue

--- routes/product.js
+++ routes/product.js
@@ -7,7 +7,7 @@ const authAll = require('../middleware/authAll');
 const {body,validationResult} = require('express-validator');
 const fileupload = require('../middleware/fileupload')
 
-route.post('/createproduct',body('name').notEmpty().withMessage('Name is required'),body('price').notEmpty().withMessage('Price is required'),body('category').notEmpty().withMessage('category is required'),authAdmin,fileupload,ProductController.createproduct);
+route.post('/createproduct',authAdmin,fileupload,body('name').notEmpty().withMessage('Name is required'),body('price').notEmpty().withMessage('Price is required'),body('category').notEmpty().withMessage('category is required'),ProductController.createproduct);
 route.get('/list',authAll,ProductController.list);
 route.put('/update/:id',authAdmin,fileupload,ProductController.update);
 route.delete('/delete/:id',authAdmin,ProductController.delete);
